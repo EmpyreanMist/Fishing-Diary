e567aced761105bafdbf87b955d4792ab9421b0f
Christian <christianfryksten1@gmail.com>
2025-10-07 13:26:59 +0200

    add stats header

--- app/(tabs)/stats.tsx
+++ app/(tabs)/stats.tsx
@@ -1,27 +1,12 @@
-import { Ionicons } from "@expo/vector-icons";
-import { LinearGradient } from "expo-linear-gradient";
-import { StyleSheet, Text, View } from "react-native";
+import { StatsHeader } from "@/components/ui/StatsHeader";
+import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function StatsScreen() {
   return (
     <SafeAreaView style={styles.safeArea} edges={["top"]}>
       <View style={styles.container}>
-        <LinearGradient
-          colors={["#2F7D71", "#2E6F83"]}
-          start={[0, 0]}
-          end={[1, 1]}
-          style={styles.header}
-        >
-          <View style={styles.headerContent}>
-            <Ionicons name="stats-chart" size={26} color="#fff" />
-            <Text style={styles.headerTitle}>Statistics</Text>
-          </View>
-
-          <Text style={styles.headerSubtitle}>
-            Track your fishing progress and achievements
-          </Text>
-        </LinearGradient>
+        <StatsHeader />
       </View>
     </SafeAreaView>
   );
@@ -35,28 +20,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#0A121A",
-  },
-  header: {
-    width: "100%",
-    height: 140,
-  },
-  headerContent: {
-    flexDirection: "row",
-    alignItems: "center",
-    height: "50%",
-    paddingHorizontal: 20,
-  },
-  headerTitle: {
-    color: "#fff",
-    fontWeight: "bold",
-    fontSize: 22,
-    marginLeft: 8,
-  },
-  headerSubtitle: {
-    color: "#fff",
-    opacity: 0.9,
-    fontSize: 18,
-    paddingHorizontal: 20,
-    marginTop: 4,
   },
 });
