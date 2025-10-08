d257ab3815c96815c30ec3b9b2c920198ecec30e
marcusmoller97 <marcusssmoller@gmail.com>
2025-10-08 10:17:42 +0200

    basic layout for addNewCatch-form, added icon to form

--- components/addCatch/CatchForm.tsx
+++ components/addCatch/CatchForm.tsx
@@ -1,7 +1,8 @@
-import { Center } from '@/components/ui/center';
 import { Heading } from '@/components/ui/heading';
+import { Icon } from '@/components/ui/icon';
 import { Input, InputField } from '@/components/ui/input';
-import { FormControl } from '@gluestack-ui/themed';
+import { VStack } from '@/components/ui/vstack';
+import { CloseIcon, FormControl, View } from '@gluestack-ui/themed';
 
 import { Box } from '@/components/ui/box';
 import { Divider } from '@/components/ui/divider';
@@ -13,15 +14,18 @@ export default function CatchForm({ stmt }: CatchBoolean) {
   if (!stmt) return null;
 
   return (
-    <Center className="absolute top-[10%] left-[5%] right-[5%] z-50 rounded-lg border border-outline-200 bg-primary-200 h-[95%] w-[90%]">
-      <FormControl className="pl-5 rounded-lg w-full h-[100%]">
-        <Box className="pt-5 pb-5  w-[80%]">
-          <Heading className="textAlign" size="2xl">
-            Add New Catch
-          </Heading>
-          <Divider className="my-0.5" />
-          <Heading size="lg"> Record your latest fishing trip </Heading>
+    <View className="bg-black/40 absolute top-[8%] left-[5%] right-[5%] z-50 rounded-lg border border-outline-200 h-[95%] w-[90%]">
+      <VStack space="$md" reversed={false}>
+        <Box className="w-[100%] h-10 pt-5 pr-5 items-end flex-end'">
+          <Icon as={CloseIcon} size="xl" color="white" className="text-typography-500" />
         </Box>
+        <Box className="pb-5 pl-5 w-[80%]">
+          <Heading size="2xl">Add New Catch</Heading>
+          <Divider />
+          <Heading size="lg">Record your latest fishing trip</Heading>
+        </Box>
+      </VStack>
+      <FormControl className="pl-5 rounded-lg w-full h-[100%] w-[100]">
         <Heading className="" size="lg">
           Add New Catch
         </Heading>
@@ -75,6 +79,6 @@ export default function CatchForm({ stmt }: CatchBoolean) {
           <InputField placeholder="Enter Text here..." />
         </Input>
       </FormControl>
-    </Center>
+    </View>
   );
 }
