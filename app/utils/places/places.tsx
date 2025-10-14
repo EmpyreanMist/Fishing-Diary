585963301b2995859bc666d74fc3cea7f12b59f1
Christian <christianfryksten1@gmail.com>
2025-10-14 11:48:18 +0200

    Added pin functionality
    Co-authored-by: marcusmoller97 <marcusmoller97@users.noreply.github.com>
    Co-authored-by: Johannes Persson <johannes.persson47@gmail.com>

---
+++ app/utils/places/places.tsx
@@ -0,0 +1,23 @@
+export const places = [
+  {
+    latitude: 59.3293,
+    longitude: 18.0686,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+    name: "Stockholm City Hall",
+  },
+  {
+    latitude: 57.7089,
+    longitude: 11.9746,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+    name: "Göteborgs Domkyrka",
+  },
+  {
+    latitude: 63.8258,
+    longitude: 20.263,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+    name: "Umeå Universitet",
+  },
+];
