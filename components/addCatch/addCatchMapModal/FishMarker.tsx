821920f1e0f951615802412e0dada989b4723884
Christian <christianfryksten1@gmail.com>
2025-11-21 15:43:26 +0100

    Fish icon now follows same style as similar icon on the map page

--- components/addCatch/addCatchMapModal/FishMarker.tsx
+++ components/addCatch/addCatchMapModal/FishMarker.tsx
@@ -1,10 +1,15 @@
 import { View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { useState } from "react";
 
 export default function FishMarker() {
   return (
-    <View>
+    <View
+      style={{
+        backgroundColor: "rgba(0,0,0,0.5)",
+        padding: 6,
+        borderRadius: 20,
+      }}
+    >
       <Ionicons name="fish" size={28} color="#4CC9F0" />
     </View>
   );
