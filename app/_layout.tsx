685e88a841f0affabc1f320620d5d085436210e0
Christian <christianfryksten1@gmail.com>
2025-11-25 17:46:31 +0100

    Remove ugly header with router placement

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -39,6 +39,7 @@ function RootNavigationGuard() {
 
   return (
     <Stack>
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen
         name="modal"
